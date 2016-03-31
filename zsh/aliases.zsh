bfa345228f3c94d6e9556b25d50b89715d5712bc
Kodie <kodiegoodwin@21.co>
2016-03-31 15:33:18 -0700

    zsh: add go alias and fix gr

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias pacup="yaourt -Syua --noconfirm"
-alias gr="cd $(git rev-parse --show-toplevel)"
+alias gr="cd '$(git rev-parse --show-toplevel)'"
 alias vim="nvim"
 alias c="cd $CODE_DIR"
 alias gf="git fetch --prune --tags"
+alias go="git checkout"
