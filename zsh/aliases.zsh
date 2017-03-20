16c31d0909a0d89fcc69397476536b70887431f5
Kodie <kodiegoodwin@gmail.com>
2017-03-20 10:27:02 -0700

    zsh/aliases: add docker aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,7 @@ alias gr="cd '$(git rev-parse --show-toplevel)'"
 alias vim="nvim"
 alias c="cd $CODE_DIR"
 alias gf="git fetch --prune --tags"
+alias gbc='git branch --merged | grep -v "\*" | grep -v master | grep -v develop | grep -v release | xargs -n 1 git branch -d && git remote prune origin'
+alias diclean='docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")'
+alias dclean='docker rm $(docker ps -a -q)'
+alias dc='docker-compose'
