14b154f2e0cfd330cb6d33c21e95e09b3487ccbf
Kodie <kodiegoodwin@gmail.com>
2017-07-14 12:15:29 -0700

    Support dock

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -21,3 +21,11 @@ fi
 if xrandr | grep "DP-1 connected"; then
     MONITOR="DP-1" polybar -c ~/.config/polybar/config enkoder &
 fi
+
+if xrandr | grep "DP-2-1 connected"; then
+    MONITOR="DP-2-1" polybar -c ~/.config/polybar/config enkoder &
+fi
+
+if xrandr | grep "DP-2-2 connected"; then
+    MONITOR="DP-2-2" polybar -c ~/.config/polybar/config enkoder &
+fi
